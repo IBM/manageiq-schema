f4939be12cf76c6f17e4affec02d45f8724481e9
GREGORYBEYER <gregoryb@il.ibm.com>
2020-09-24 15:31:37 +0300

    SDE-1385: [WIP] bugfixes 2
    Removed some remaining "storage_system" references.
    finished renaming storage_system_family -> physical_storage_family
    Added missing reference to physical_storage table

--- db/migrate/20200608163713_create_physical_storage_families.rb
+++ db/migrate/20200608163713_create_physical_storage_families.rb
@@ -1,6 +1,6 @@
-class CreateStorageSystemFamilies < ActiveRecord::Migration[5.2]
+class CreatePhysicalStorageFamilies < ActiveRecord::Migration[5.2]
   def change
-    create_table :storage_system_families do |t|
+    create_table :physical_storage_families do |t|
       t.string :name
       t.string :version
       t.references :ems, :type => :bigint, :index => true, :references => :ext_management_system
--- db/migrate/20200922180455_add_uuid_to_physical_storages.rb
+++ db/migrate/20200922180455_add_uuid_to_physical_storages.rb
@@ -1,7 +1,7 @@
-class AddUuidToPhysicalStorages < ActiveRecord::Migration[5.0]
+class AddUuidToPhysicalStorages < ActiveRecord::Migration[5.2]
   def change
     add_column :physical_storages, :uuid, :string
-    add_column :physical_storages, :storage_family, :string
+    add_reference :physical_storages, :physical_storage_family, :type => :bigint, :index => true
   end
 end
 
